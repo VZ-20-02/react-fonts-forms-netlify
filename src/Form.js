a9374c752450fb2bc81f913ebaa7981301244133
GFSTee <georg.treitz@gmail.com>
2021-01-29 10:17:20 +0100

    logo rem

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,7 @@
 const Form = () => {
     return (<>
         <form name="contact v1" method="post">
+            {/* Wichtig ist das versteckte Inputfeld und dessen value, der eine Verbindung zu dem Form im HTML herstellt */}
             <input type="hidden" name="form-name" value="contact v1" /> <br />
             <input type="text" name="first-name" placeholder="First Name" /><br />
             <input type="email" name="email" placeholder="Email" /><br />
